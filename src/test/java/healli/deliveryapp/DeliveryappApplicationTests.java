d4e552aa0f353d4e76d44bebaaf185df0192fcc4
Eduardo Henrique <eng.eduardohealli@gmail.com>
2024-05-11 15:54:15 -0300

    initial commit

---
+++ src/test/java/healli/deliveryapp/DeliveryappApplicationTests.java
@@ -0,0 +1,13 @@
+package healli.deliveryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
